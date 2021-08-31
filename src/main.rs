0b8c8399b8da0186a4e5f34b14713e0cdff2f19f
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2021-08-31 19:32:11 +0200

    WIP: the box game example launch without panicking
    It still doesn't work.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,97 @@
-use bevy::prelude::*;
+use bevy::{core::FixedTimestep, prelude::*};
+use std::net::SocketAddr;
+
+use bevy_ggrs::{GGRSApp, GGRSPlugin};
+use ggrs::PlayerType;
+
+mod args;
+mod box_game;
+
+use args::*;
+use box_game::*;
+
+const INPUT_SIZE: usize = std::mem::size_of::<u8>();
+const FPS: u32 = 60;
 
 fn main() {
+    // When building for WASM, print panics to the browser console
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+
+    real_main().expect("FAILED");
+}
+
+fn real_main() -> Result<(), Box<dyn std::error::Error>> {
+    // read cmd line arguments
+    let opt = Args::get();
+
+    let mut local_handle = 0;
+    let num_players = opt.players.len();
+    assert!(num_players > 0);
+
+    // create a GGRS P2P session
+    let mut p2p_sess = ggrs::start_p2p_session(num_players as u32, INPUT_SIZE, opt.local_port)?;
+
+    // turn on sparse saving
+    p2p_sess.set_sparse_saving(true)?;
+
+    // add players
+    for (i, player_addr) in opt.players.iter().enumerate() {
+        // local player
+        if player_addr == "localhost" {
+            p2p_sess.add_player(PlayerType::Local, i)?;
+            local_handle = i;
+        } else {
+            // remote players
+            let remote_addr: SocketAddr = player_addr.parse()?;
+            p2p_sess.add_player(PlayerType::Remote(remote_addr), i)?;
+        }
+    }
+    // optionally, add spectators
+    for (i, spec_addr) in opt.spectators.iter().enumerate() {
+        p2p_sess.add_player(PlayerType::Spectator(*spec_addr), num_players + i)?;
+    }
+
+    // set input delay for the local player
+    p2p_sess.set_frame_delay(2, local_handle)?;
+
+    // set default expected update frequency (affects synchronization timings between players)
+    p2p_sess.set_fps(FPS)?;
+
+    // start the GGRS session
+    p2p_sess.start_session()?;
+
     let mut app = App::new();
+
     app.insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins);
+
     #[cfg(target_arch = "wasm32")]
     app.add_plugin(bevy_webgl2::WebGL2Plugin);
-    app.add_startup_system(setup.system()).run();
+
+    app.add_startup_system(setup_system.system());
+
+    app.add_plugin(GGRSPlugin);
+
+    // add your GGRS session
+    app.with_p2p_session(p2p_sess);
+    // define frequency of game logic update
+    app.with_rollback_run_criteria(FixedTimestep::steps_per_second(FPS as f64))
+        // define system that represents your inputs as a byte vector, so GGRS can send the inputs around
+        .with_input_system(input.system())
+        // register components that will be loaded/saved
+        .register_rollback_type::<Transform>()
+        .register_rollback_type::<Velocity>()
+        // you can also register resources
+        .insert_resource(FrameCount { frame: 0 })
+        .register_rollback_type::<FrameCount>()
+        // these systems will be executed as part of the advance frame update
+        .add_rollback_system(move_cube_system)
+        .add_rollback_system(increase_frame_system);
+
+    app.run();
+
+    Ok(())
 }
 
 /// set up a simple 3D scene
@@ -41,92 +126,3 @@ fn setup(
         ..Default::default()
     });
 }
-
-// use std::net::SocketAddr;
-
-// use bevy::{core::FixedTimestep, prelude::*};
-// use bevy_ggrs::{GGRSApp, GGRSPlugin};
-// use ggrs::PlayerType;
-// use structopt::StructOpt;
-
-// mod box_game;
-// use box_game::*;
-
-// const INPUT_SIZE: usize = std::mem::size_of::<u8>();
-// const FPS: u32 = 60;
-
-// // structopt will read command line parameters for u
-// #[derive(StructOpt)]
-// struct Opt {
-//     #[structopt(short, long)]
-//     local_port: u16,
-//     #[structopt(short, long)]
-//     players: Vec<String>,
-//     #[structopt(short, long)]
-//     spectators: Vec<SocketAddr>,
-// }
-
-// fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     // read cmd line arguments
-//     let opt = Opt::from_args();
-//     let mut local_handle = 0;
-//     let num_players = opt.players.len();
-//     assert!(num_players > 0);
-
-//     // create a GGRS P2P session
-//     let mut p2p_sess = ggrs::start_p2p_session(num_players as u32, INPUT_SIZE, opt.local_port)?;
-
-//     // turn on sparse saving
-//     p2p_sess.set_sparse_saving(true)?;
-
-//     // add players
-//     for (i, player_addr) in opt.players.iter().enumerate() {
-//         // local player
-//         if player_addr == "localhost" {
-//             p2p_sess.add_player(PlayerType::Local, i)?;
-//             local_handle = i;
-//         } else {
-//             // remote players
-//             let remote_addr: SocketAddr = player_addr.parse()?;
-//             p2p_sess.add_player(PlayerType::Remote(remote_addr), i)?;
-//         }
-//     }
-
-//     // optionally, add spectators
-//     for (i, spec_addr) in opt.spectators.iter().enumerate() {
-//         p2p_sess.add_player(PlayerType::Spectator(*spec_addr), num_players + i)?;
-//     }
-
-//     // set input delay for the local player
-//     p2p_sess.set_frame_delay(2, local_handle)?;
-
-//     // set default expected update frequency (affects synchronization timings between players)
-//     p2p_sess.set_fps(FPS)?;
-
-//     // start the GGRS session
-//     p2p_sess.start_session()?;
-
-//     App::new()
-//         .insert_resource(Msaa { samples: 4 })
-//         .add_plugins(DefaultPlugins)
-//         .add_plugin(GGRSPlugin)
-//         .add_startup_system(setup_system)
-//         // add your GGRS session
-//         .with_p2p_session(p2p_sess)
-//         // define frequency of game logic update
-//         .with_rollback_run_criteria(FixedTimestep::steps_per_second(FPS as f64))
-//         // define system that represents your inputs as a byte vector, so GGRS can send the inputs around
-//         .with_input_system(input.system())
-//         // register components that will be loaded/saved
-//         .register_rollback_type::<Transform>()
-//         .register_rollback_type::<Velocity>()
-//         // you can also register resources
-//         .insert_resource(FrameCount { frame: 0 })
-//         .register_rollback_type::<FrameCount>()
-//         // these systems will be executed as part of the advance frame update
-//         .add_rollback_system(move_cube_system)
-//         .add_rollback_system(increase_frame_system)
-//         .run();
-
-//     Ok(())
-// }
